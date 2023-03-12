b6f59d03056d514eb536984ff7e2d18219fd63f8
omitsuhashi <osamu.mitsuhashi224@gmail.com>
2023-03-13 02:53:30 +0900

    add zshrc linker

--- linker.sh
+++ linker.sh
@@ -5,6 +5,7 @@ fi
 
 ln -sf ~/dotfiles/ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/zprofile ~/.zprofile
+ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/xvimrc ~/.xvimrc
 ln -sfn ~/dotfiles/vim ~/.vim
 
